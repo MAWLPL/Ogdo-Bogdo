c085e519ee29c2a754fb5a7d39618ed0a643f9a9
Maksymilian Wloch <maxwloch@interia.pl>
2023-10-29 14:10:38 +0100

    Plik

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    int number_one, number_two;
+    scanf("%d %d, &number_one, &number_two");
+    printf("The sume is equal %d\n, number_one+number_two");
+
+return 0;
+}
